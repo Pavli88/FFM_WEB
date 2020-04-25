dc77f71eeefb4fa85a9e6f5f4d3bc3ed20c8a191
apavlics <pavlicsekati@gmail.com>
2020-04-25 18:10:06 +0200

    Adding new comment line 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,6 +83,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # If I make new migrations in the TEST environment's manage.py file with the production python engine then
 # it will updated ffm_system_test database
 # New comment
+# And new
 credentials = Credentials().db_parameters
 DATABASES = credentials
 
