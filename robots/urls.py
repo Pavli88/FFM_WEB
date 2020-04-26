b24616086a006f65f49eea169b85bcc3aad49d94
apavlics <pavlicsekati@gmail.com>
2020-04-26 21:21:31 +0200

    Amending urls

--- robots/urls.py
+++ robots/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
-from mysite.views import home
 
 urlpatterns = [
-    path('', home, name="home_page"),
     path('robots/', views.robots_main, name="robots main"),
     path('robots/trade_signals', views.execute_trade),
     path('robots/create_page', views.create_robot_page, name="go to create robot"),
