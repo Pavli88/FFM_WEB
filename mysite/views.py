2f4ab1d19a41a04789201048bd3c109fa68e6910
apavlics <pavlicsekati@gmail.com>
2020-04-27 07:58:50 +0200

    Creating forms.py file in the mysite dir and adding to the main template for test purpose

--- mysite/views.py
+++ mysite/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+from mysite.forms import RobotParams
 
 
 # Home page
 def home(request):
-    return render(request, 'home.html')
+    robot_form = RobotParams()
+    return render(request, 'home.html', {"robot_form": robot_form})
 
 
 
