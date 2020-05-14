91ea848d19ff2ee07d6e91f0c15db34190d9351f
pavliati <pavlicsekati@gmail.com>
2020-05-14 19:58:02 +0200

    Adding cumulative calculation on main page

--- mysite/views.py
+++ mysite/views.py
@@ -7,6 +7,14 @@ from robots.processes.robot_processes import *
 import pandas as pd
 
 
+# Python code to get the Cumulative sum of a list
+def cumulative(lists):
+    cu_list = []
+    length = len(lists)
+    cu_list = [sum(lists[0:x:1]) for x in range(0, length+1)]
+    return cu_list[1:]
+
+
 # Home page
 def home(request):
     return render(request, 'home.html')
@@ -18,15 +26,14 @@ def get_results(request):
     trade_df = pd.DataFrame(list(trades))
     pnls = list(trade_df["open_price"]-trade_df["close_price"])
     pnl_label = [label for label in range(len(pnls))]
+    cum_pnl = cumulative(pnls)
 
     print(pnls)
     print(pnl_label)
 
-
-
-
     return render(request, 'home.html', {"pnls": pnls,
-                                         "pnl_label": pnl_label})
+                                         "pnl_label": pnl_label,
+                                         "cum_pnl": cum_pnl})
 
 
 
