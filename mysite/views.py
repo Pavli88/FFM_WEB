694ff3ff67e858e72aa2257d09a51169d6da9ad8
pavliati <pavlicsekati@gmail.com>
2020-05-15 13:42:22 +0200

    Working on main html

--- mysite/views.py
+++ mysite/views.py
@@ -9,24 +9,53 @@ from robots.models import *
 
 # Python code to get the Cumulative sum of a list
 def cumulative(lists):
+
+    """
+    Function that creates cumulative list based on given list
+    :param lists:
+    :return:
+    """
+
     cu_list = []
     length = len(lists)
     cu_list = [sum(lists[0:x:1]) for x in range(0, length+1)]
     return cu_list[1:]
 
 
-# Home page
-def home(request):
+def get_today():
+
+    """
+    This function gets today's value and returns it back as a string
+    :return:
+    """
+
     today = date.today()
+    return str(today)
+
+
+def get_robot_list():
+
+    """
+    Function to get a list of robots from the db.
+    :return:
+    """
+
     robots = Robots.objects.filter().values()
     robot_df = pd.DataFrame(list(robots))
     robot_list = list(robot_df["name"])
     robot_list.append("ALL")
 
-    print("Loading robot list from db:", robot_list)
+    return robot_list
 
-    return render(request, 'home.html', {"today": str(today),
-                                         "robots": robot_list})
+
+# Home page
+def home(request):
+
+    print("Loading robot list from db:", get_robot_list())
+
+    return render(request, 'home.html', {"today": get_today(),
+                                         "robots": get_robot_list(),
+                                         "message": ""})
 
 
 def get_results(request):
@@ -41,25 +70,28 @@ def get_results(request):
         print("Robot Name:", robot_name)
         print("Start Date: ", start_date)
 
-    today = date.today()
-    print("Today's date:", today)
+    print("Today's date:", get_today())
 
     if trade_side == "ALL" and robot_name == "ALL":
-        trades = Trades.objects.filter(status="CLOSE").values()
+        trades = Trades.objects.filter(status="CLOSE",
+                                       close_time__range=[start_date, get_today()]).values()
     elif robot_name == "ALL":
         trades = Trades.objects.filter(status="CLOSE",
-                                       side=trade_side).values()
+                                       side=trade_side,
+                                       close_time__range=[start_date, get_today()]).values()
     else:
         trades = Trades.objects.filter(status="CLOSE",
                                        side=trade_side,
                                        robot=robot_name,
-                                       close_time__range=[start_date, str(today)]).values()
+                                       close_time__range=[start_date, get_today()]).values()
 
     trade_df = pd.DataFrame(list(trades))
 
     if trade_df.empty is True:
         print("Empty Dataframe")
-        return render(request, 'home.html', {"message": "true"})
+        return render(request, 'home.html', {"today": get_today(),
+                                             "robots": get_robot_list(),
+                                             "message": "Record does not exists"})
 
     open_prices = list(trade_df["open_price"])
     close_prices = list(trade_df["close_price"])
@@ -86,7 +118,10 @@ def get_results(request):
 
     return render(request, 'home.html', {"pnls": pnls,
                                          "pnl_label": pnl_label,
-                                         "cum_pnl": cum_pnl})
+                                         "cum_pnl": cum_pnl,
+                                         "today": get_today(),
+                                         "robots": get_robot_list(),
+                                         "message": ""})
 
 
 
