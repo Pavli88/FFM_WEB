40d8216d8ffe15abdb24655536740b3e3cfcd350
Pavli88 <pavlicsekati@gmail.com>
2020-10-06 21:14:21 +0200

    adding amended trade views file

--- trade_app/views.py
+++ trade_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Main site for trading
 def trade_main(request):
-    return render(request, 'trade/trade_main.html')
+    return render(request, 'trade_app/trade_main.html')
