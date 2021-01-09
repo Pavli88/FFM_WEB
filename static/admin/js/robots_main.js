9d2c0392e8f86ca0dd6f1e20a9203ca216e8b744
Pavli88 <pavlicsekati@gmail.com>
2021-01-09 18:06:35 +0100

    Working on restructuring front end elements

--- static/admin/js/robots_main.js
+++ static/admin/js/robots_main.js
@@ -1,58 +1,11 @@
 // Loading default parameters
-let now = new Date()
-let day = ("0" + now.getDate()).slice(-2)
-let month = ("0" + (now.getMonth() + 1)).slice(-2)
-let today = now.getFullYear()+"-"+(month)+"-"+(day)
-let firstDay = now.getFullYear()+"-"+(month)+"-01"
-$("#processStartDate").val(firstDay)
-$("#processEndDate").val(today)
-
-// Robot process calculation panel
-let calcButton = $("#calc_btn")
-let process = $("#robot_proc_selector")
-let robot = $("#robot")
-let startDate = $("#processStartDate")
-let endDate = $("#processEndDate")
-let calcLoadingDiv = $("#calcLoadingDiv")
-let robotMessageTable = $("#robotTableMessageBody")
-
-calcLoadingDiv.hide()
-calcButton.click(robotProcess)
-
-function robotProcess() {
-    calcLoadingDiv.show()
-    try {
-        robotMessageTable.empty()
-    } catch (err) {
-    }
-    $.ajax({
-        type: "POST",
-        url: "process_hub/",
-        data: {
-            csrfmiddlewaretoken: $('meta[name="csrf-token"]').attr('content'),
-            process: process.val(),
-            robot: robot.val(),
-            date: startDate.val(),
-            endDate: endDate.val(),
-        },
-        success: function (response) {
-
-            for (let msg of response["message"]){
-                let newMessagTr = document.createElement("tr")
-                let newMessage = document.createElement("td")
-                let icon = document.createElement("td")
-
-                newMessagTr.classList.add("robot_table_row")
-                newMessage.innerText = msg
-                newMessagTr.append(icon)
-                newMessagTr.append(newMessage)
-                robotMessageTable.append(newMessagTr)
-
-            }
-            calcLoadingDiv.hide()
-        }
-    })
-}
+// let now = new Date()
+// let day = ("0" + now.getDate()).slice(-2)
+// let month = ("0" + (now.getMonth() + 1)).slice(-2)
+// let today = now.getFullYear()+"-"+(month)+"-"+(day)
+// let firstDay = now.getFullYear()+"-"+(month)+"-01"
+// $("#processStartDate").val(firstDay)
+// $("#processEndDate").val(today)
 
 // Loading robots to table
 let loadButton = $("#load_robots")
