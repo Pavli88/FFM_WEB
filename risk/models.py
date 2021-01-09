18e96b63d0cd2dd44d4aec032ba367939dfe56f7
Pavli88 <pavlicsekati@gmail.com>
2021-01-09 19:05:28 +0100

    Working on risk back end and layout

--- risk/models.py
+++ risk/models.py
@@ -9,3 +9,4 @@ class AccountRisk(models.Model):
 class RobotRisk(models.Model):
     robot = models.CharField(max_length=50, unique=True, default="")
     daily_risk_perc = models.FloatField(default=0.00)
+    daily_trade_limit = models.FloatField(default=0.00)
