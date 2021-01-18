d8470cf8cc611886278a5180b332d0df86e869fe
Pavli88 <pavlicsekati@gmail.com>
2021-01-19 00:56:01 +0100

    Amending job file

--- mysite/jobs.py
+++ mysite/jobs.py
@@ -1,7 +1,4 @@
-from robots.processes.robot_processes import *
-from mysite.my_functions.general_functions import *
 import requests
-
 
 r = requests.get("http://pavliati.pythonanywhere.com/robots/calculate_robot_balance/")
 
