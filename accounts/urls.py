a6b5a878c1d12b1428a3dc5cf9032e14184a7564
Pavli88 <pavlicsekati@gmail.com>
2021-07-07 20:31:35 +0200

    Updating account views and url routing

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from accounts.views import *
 
 urlpatterns = [
-    path('accounts/create_account/', create_broker, name="create broker account"),
-    path('accounts/new_cash_flow/', new_cash_flow),
-    path('accounts/get_accounts/', new_cash_flow),
+    path('accounts/new_account/', create_broker),
     path('accounts/get_accounts_data/', get_accounts_data),
     path('accounts/get_account_data/', get_account_data),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,8 +3,11 @@ from django.http import HttpResponse
 from accounts.models import *
 from accounts.account_functions import *
 from django.http import JsonResponse
-from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+import json
 
+
+@csrf_exempt
 def create_broker(request):
 
     """
@@ -16,10 +19,12 @@ def create_broker(request):
     print("New broker creation request received.")
 
     if request.method == "POST":
-        broker_name = request.POST.get("broker_name")
-        account_number = request.POST.get("account_number")
-        environment = request.POST.get("env")
-        token = request.POST.get("token")
+        request_data = json.loads(request.body.decode('utf-8'))
+        broker_name = request_data['broker_name']
+        account_number = request_data['account_number']
+        environment = request_data['env']
+        token = request_data['token']
+        currency = request_data['currency']
 
     print("BROKER", broker_name)
     print("ACCOUNT NUMBER", account_number)
@@ -30,42 +35,22 @@ def create_broker(request):
         BrokerAccounts(broker_name=broker_name,
                        account_number=account_number,
                        access_token=token,
-                       env=environment).save()
+                       env=environment,
+                       currency=currency).save()
 
         print("Inserting new account to database")
 
         print("Setting up initial balance to zero")
 
-        AccountBalance(account_number=account_number).save()
-
-        response = "not exists"
+        response = "Account is created successfully!"
     except:
         print("Account exists in database")
-        response = "exists"
+        response = "Account already exists in the database!"
 
     print("Sending response to front end")
     print("")
 
     return JsonResponse(response, safe=False)
-
-
-def get_accounts():
-
-    """
-    Queries out all accounts from database and passes it back to the html
-    :param request:
-    :return:
-    """
-    accounts = BrokerAccounts.objects.filter().values()
-
-    return accounts
-
-
-def get_account_balances():
-
-    balances = AccountBalance.objects.filter().values()
-
-    return balances
 
 
 def get_account_data(request, account):
@@ -105,46 +90,3 @@ def get_accounts_data(request):
 
         return JsonResponse(response, safe=False)
 
-
-def new_cash_flow(request):
-    print("*** NEW ACCOUNT CASHFLOW ***")
-
-    if request.method == "POST":
-        account = request.POST.get("account")
-        cash_flow = request.POST.get("cash_flow")
-
-    print("ACCOUNT", account)
-    print("CASH FLOW", cash_flow)
-    print("Entering cashflow into the database")
-
-    AccountCashFlow(account_number=account,
-                    cash_flow=cash_flow).save()
-
-    response = "New cash flow was entered into the database"
-
-    print("Sending response to front end")
-
-    return JsonResponse(response, safe=False)
-
-
-# def get_account_data(request, data_type):
-#     print("*** GET ACCOUNT DATA REQUEST ***")
-#     print("DATA TYPE REQUEST:", data_type)
-#
-#     if request.method == "GET":
-#         account = request.GET.get("account")
-#         start_date = request.GET.get("start_date")
-#         end_date = request.GET.get("end_date")
-#
-#     print("ACCOUNT:", account)
-#     print("START DATE:", start_date)
-#     print("END DATE:", end_date)
-#
-#     if data_type == "balance":
-#         response_data = get_account_balance_history(account=account, start_date=start_date, end_date=end_date)
-#
-#     response = {"message": list(response_data)}
-#
-#     print("Sending message to front end")
-#
-#     return JsonResponse(response, safe=False)
