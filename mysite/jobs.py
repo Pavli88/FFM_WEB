fae859083831eccdf956a50464d0c775dadd8104
Pavli88 <pavlicsekati@gmail.com>
2022-08-19 09:19:30 +0200

    Amending jobs file

--- mysite/jobs.py
+++ mysite/jobs.py
@@ -7,11 +7,11 @@ r = requests.post("http://pavliati.pythonanywhere.com/robots/calculate_robot_bal
 print("Robot balance calculation finished!")
 
 print("PORTFOLIO POSITIONS")
-r = requests.post("http://pavliati.pythonanywhere.com/portfolios/positions/", json={'portfolio': 'ALL', 'start_date': str(date.today()), 'end_date': str(date.today())})
+# r = requests.post("http://pavliati.pythonanywhere.com/portfolios/positions/", json={'portfolio': 'ALL', 'start_date': str(date.today()), 'end_date': str(date.today())})
 print("Portfolio positions calculation finished!")
 
 print("PORTFOLIO CASHFLOW")
-r = requests.post("http://pavliati.pythonanywhere.com/portfolios/cash_holding/", json={'portfolio': 'ALL', 'start_date': str(date.today()), 'end_date': str(date.today())})
+# r = requests.post("http://pavliati.pythonanywhere.com/portfolios/cash_holding/", json={'portfolio': 'ALL', 'start_date': str(date.today()), 'end_date': str(date.today())})
 print("Portfolio cashflow calculation finished!")
 
 # r = requests.post("http://127.0.0.1:8000/robots/calculate_robot_balance/", json={'robot': 'ALL', 'start_date': str(date.today()), 'end_date': str(date.today())})
