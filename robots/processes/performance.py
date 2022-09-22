7b1bb1a32a325d650a1b2105ff30e82e006cec3c
pavliati <pavlicsekati@gmail.com>
2022-09-23 00:50:35 +0200

    Fix bug

--- robots/processes/performance.py
+++ robots/processes/performance.py
@@ -9,14 +9,13 @@ def monthly_return_calc(robot_code, start_date, end_date):
     balances = pd.DataFrame(Balance.objects.filter(robot_name=robot_code,
                                                    date__gte=start_date,
                                                    date__lte=end_date).values())
-    print(balances)
+    # print(balances)
     if balances.empty:
         return 'No balance'
-    pnls = balances['realized_pnl'].sum()
     total_cash_flow = balances['cash_flow'].sum()
     opening_balance = list(balances['opening_balance'])[0]
     closing_balance = list(balances['close_balance'])[-1]
-    performance = round((closing_balance + pnls - total_cash_flow) / opening_balance, 5)
+    performance = round((closing_balance - opening_balance - total_cash_flow) / opening_balance, 5)
     try:
         monthly_return = MonthlyReturns.objects.get(robot_code=robot_code, date=end_date)
         monthly_return.ret = performance
