3b4fa10716f7fdeddbff2736cb89aec5dcc0463f
pavliati <pavlicsekati@gmail.com>
2024-10-22 00:12:34 +0200

    fed

--- mysite/settings.py
+++ mysite/settings.py
@@ -173,6 +173,6 @@ STATICFILES_DIRS = [
 # default static files settings for PythonAnywhere.
 MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_URL = "/home/"
 
