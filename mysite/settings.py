233f1c9e11bf61346377365715187f81ec4eb454
pavliati <pavlicsekati@gmail.com>
2024-10-21 21:50:19 +0200

    fed

--- mysite/settings.py
+++ mysite/settings.py
@@ -2,6 +2,7 @@ import os
 import sys
 from django.core.management.utils import get_random_secret_key
 from mysite.credentials import *
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
