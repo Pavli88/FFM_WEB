65a063d352250abe86ce7d3266897192997666b0
pavliati <pavlicsekati@gmail.com>
2025-04-30 22:32:05 +0200

    fixing the issue when broker credentials were not filtered for the user and loaded all credentials data during trade exectuion.

--- trade_app/views_folder/create_view.py
+++ trade_app/views_folder/create_view.py
@@ -31,7 +31,7 @@ class TradeExecution:
         self.broker = Brokers.objects.get(id=self.account.broker_id)
 
         try:
-            self.broker_credentials = BrokerCredentials.objects.get(broker=self.account.broker)
+            self.broker_credentials = BrokerCredentials.objects.get(broker=self.account.broker, user=self.account.user)
         except BrokerCredentials.DoesNotExist:
             Notifications(portfolio_code=self.portfolio.portfolio_code,
                           message='Rejected Trade. Missing Credentials: ' + str(self.broker.broker_code),
@@ -299,4 +299,117 @@ def close_trade_by_id(request):
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def new_trade(request):
-    return JsonResponse({}, safe=False)
+    if request.method == "POST":
+        request_body = json.loads(request.body.decode('utf-8'))
+        print(request_body)
+
+        # OLD Signal
+        # request_body = {'portfolio_code': 'TST',
+        #                 'account_id': 6,
+        #                 'security': 74,
+        #                 'transaction_type': 'Close',
+        #                 'quantity': 1,
+        #                 }
+        # NEW Signal
+        # request_body = {'portfolio_code': 'TST',
+        #                 'security': 74,
+        #                 'transaction_type': 'Close',
+        #                 'quantity': 1,
+        #                 }
+
+        # Close transaction from the Risk view
+        if request_body['transaction_type'] == 'risk_closure':
+            transactions = pd.DataFrame(Transaction.objects.filter(id__in=request_body['ids']).values())
+            for index, transaction in transactions.iterrows():
+                execution = TradeExecution(portfolio_code=transaction['portfolio_code'],
+                                           account_id=transaction['account_id'],
+                                           security=transaction['security'],
+                                           )
+                closed_transactions = pd.DataFrame(
+                    Transaction.objects.filter(transaction_link_code=transaction['id']).values())
+                if len(closed_transactions) == 0:
+                    closed_out_units = 0
+                else:
+                    closed_out_units = closed_transactions['quantity'].sum()
+                quantity = transaction['quantity'] - abs(closed_out_units)
+                execution.close(transaction=transaction, quantity=quantity)
+            return JsonResponse('Transactions are closed', safe=False)
+
+        # Checking if trade routing exists
+        try:
+            routing = TradeRoutes.objects.get(portfolio_code=request_body['portfolio_code'],
+                                              inst_id=request_body['security_id'])
+        except:
+            Notifications(portfolio_code=request_body['portfolio_code'],
+                          message='Missing trade routing. Security Code: ' + str(request_body['security_id']),
+                          sub_message='Error',
+                          broker_name='-').save()
+            return JsonResponse({}, safe=False)
+
+        if routing.is_active == 0:
+            Notifications(portfolio_code=request_body['portfolio_code'],
+                          message='Rejected Trade. Inactive Routing. Security: ' + str(request_body['security_id']),
+                          sub_message='Inactive trade routing',
+                          broker_name='-').save()
+            return JsonResponse({}, safe=False)
+
+        # This code determines if trade routing quantity has to be applied or manual quantity multiplier
+        if 'manual' in request_body:
+            quantity_multiplier = 1
+            account_id = request_body['account_id']
+        else:
+            quantity_multiplier = routing.quantity
+            account_id = routing.broker_account_id
+
+        # Initalizing trade execution
+        # Account ID determines the broker and the broker account to trade on
+        try:
+            execution = TradeExecution(
+                portfolio_code=request_body['portfolio_code'],
+                account_id=request_body['account_id'],
+                security_id=request_body['security_id']
+            )
+        except ValueError as e:
+            return JsonResponse({'response': str(e)})
+
+        if request_body['transaction_type'] == "Close All":
+            open_transactions = Transaction.objects.filter(security=request_body['security_id'],
+                                                           is_active=1,
+                                                           portfolio_code=request_body['portfolio_code'])
+            for transaction in open_transactions:
+                execution.close(transaction=transaction)
+
+        # Liquidating the whole portfolio and open trades at the same time
+        elif request_body['transaction_type'] == 'Liquidate':
+            open_transactions = Transaction.objects.filter(is_active=1,
+                                                           portfolio_code=request_body['portfolio_code'])
+            for transaction in open_transactions:
+                execution.close(transaction=transaction)
+
+        # Partial close out of an existing position
+        elif request_body['transaction_type'] == 'Close Out':
+            transaction = Transaction.objects.get(id=request_body['id'])
+            execution.close(transaction=transaction, quantity=request_body['quantity'])
+
+        # Closing a single open trade with the rest outstanding quantity
+        elif request_body['transaction_type'] == 'Close':
+            transaction = Transaction.objects.get(id=request_body['id'])
+            # print('PARENT')
+            # print(transaction)
+            execution.close(transaction=transaction)
+
+        # This is the trade execution
+        else:
+            if 'sl' in request_body:
+                latest_nav = list(
+                    Nav.objects.filter(portfolio_code=request_body['portfolio_code']).order_by('date').values_list(
+                        'total', flat=True))[-1]
+                risked_amount = latest_nav * float(request_body['risk_perc'])
+                current_price = execution.get_market_price()
+                price_diff = abs(current_price - float(request_body['sl']))
+                quantity = str(int(risked_amount / price_diff))
+                request_body['quantity'] = quantity
+            execution.new_trade(transaction_type=request_body['transaction_type'],
+                                quantity=float(request_body['quantity']) * quantity_multiplier)
+        # calculate_holdings(portfolio_code=request_body['portfolio_code'], calc_date=trade_date)
+        return JsonResponse({}, safe=False)
